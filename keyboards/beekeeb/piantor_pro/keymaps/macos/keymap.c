970637cebec697941490a21ea812a2e10965f22c
jocarino <joao.goncalo.veloso@gmail.com>
2025-07-02 11:27:12 +0100

    feat: add cmd + num

--- keyboards/beekeeb/piantor_pro/keymaps/macos/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/macos/keymap.c
@@ -42,16 +42,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return true;             // Return true for normal processing of tap keycode
         case LT(0,KC_QUOT): // '@
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_QUOT); // Intercept tap function to send '
-            } else if (record->event.pressed) {
+            if (!record->tap.count && record->event.pressed) {
                 tap_code16(S(KC_QUOT)); // Intercept hold function to send @
             }
             return false;
         case LT(1,KC_QUOT): // '@
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_QUOT); // Intercept tap function to send '
-            } else if (record->event.pressed) {
+            if (!record->tap.count && record->event.pressed) {
                 tap_code16(S(KC_QUOT)); // Intercept hold function to send @
             }
             return false;
@@ -67,6 +63,66 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             }
             return true;
+        case LT(2,KC_1): // Press -> 1 ; hold CMD+1
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(LGUI(KC_1));
+                return false;
+            }
+            return true;
+        case LT(2,KC_2): // Press -> 2 ; hold CMD+2
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(LGUI(KC_2));
+                return false;
+            }
+            return true;
+        case LT(2,KC_3): // Press -> 3 ; hold CMD+3
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(LGUI(KC_3));
+                return false;
+            }
+            return true;
+        case LT(2,KC_4): // Press -> 4 ; hold CMD+4
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(LGUI(KC_4));
+                return false;
+            }
+            return true;
+        case LT(2,KC_5): // Press -> 5 ; hold CMD+5
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(LGUI(KC_5));
+                return false;
+            }
+            return true;
+        case LT(2,KC_6): // Press -> 6 ; hold CMD+6
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(LGUI(KC_6));
+                return false;
+            }
+            return true;
+        case LT(2,KC_7): // Press -> 7 ; hold CMD+7
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(LGUI(KC_7));
+                return false;
+            }
+            return true;
+        case LT(2,KC_8): // Press -> 8 ; hold CMD+8
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(LGUI(KC_8));
+                return false;
+            }
+            return true;
+        case LT(2,KC_9): // Press -> 9 ; hold CMD+9
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(LGUI(KC_9));
+                return false;
+            }
+            return true;
+        case LT(2,KC_0): // Press -> 0 ; hold CMD+0
+            if (!record->tap.count && record->event.pressed) {
+                tap_code16(LGUI(KC_0));
+                return false;
+            }
+            return true;
     }   
     return true;
 }
@@ -75,19 +131,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [0] = LAYOUT_split_3x6_3(
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,               KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL,
         KC_LALT, KC_A, KC_S, KC_D, KC_F, KC_G,              KC_H, KC_J, KC_K, KC_L, LT(0,KC_SCLN), LT(0,KC_QUOT),
-        KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,              KC_N, KC_M, LT(0,KC_COMM), LT(0,KC_DOT), LT(0,KC_SLSH), KC_ESC,
+        KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,              KC_N, KC_M, LT(0,KC_COMM), LT(0,KC_DOT), LT(0,KC_SLSH), KC_LGUI,
                     KC_LSFT, HYPR_T(KC_SPC), MO(1), MO(2), LGUI_T(KC_ENT), KC_BSPC
         ),
         [1] = LAYOUT_split_3x6_3(
         KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                    KC_CIRC, KC_AMPR, KC_PAST, LT(1,KC_MINS), LT(1,KC_EQL), LALT(KC_BSPC),
         KC_LALT, LGUI(KC_A), LGUI(KC_S), KC_TAB, KC_LALT, KC_LSFT,               KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, LT(1,KC_QUOT), KC_HASH,
         KC_LCTL, LGUI(KC_Z), LGUI(KC_X), LGUI(KC_C), LGUI(KC_V), LALT(KC_SPC),     KC_NO, KC_HOME, KC_END, KC_PGUP, KC_PGDN, KC_NO,
-                                            KC_LSFT, KC_SPC, KC_NO, MO(3), KC_ESC, KC_LGUI
+                                            KC_LSFT, KC_LGUI, KC_NO, MO(3), KC_ESC, KC_LGUI
         ),
         [2] = LAYOUT_split_3x6_3(
-        KC_TAB, KC_GRV, KC_1, KC_2, KC_3, KC_NO,                 KC_NO, KC_LCBR, KC_RCBR, KC_NO, KC_NO, KC_NO,
-        KC_LALT, LT(2,KC_NUHS), KC_4, KC_5, KC_6, KC_NO,          KC_NO, KC_LPRN, KC_RPRN, KC_NO, KC_NO, KC_NO,
-        KC_LCTL, LT(2,KC_NUBS), KC_7, KC_8, KC_9, KC_0,           KC_NO, KC_LBRC, KC_RBRC, KC_COMM, KC_GT, KC_NO,
+        KC_TAB, KC_GRV, LT(2,KC_1), LT(2,KC_2), LT(2,KC_3), LGUI(KC_PLUS),                 KC_NO, KC_LCBR, KC_RCBR, KC_NO, KC_NO, KC_NO,
+        KC_LALT, LT(2,KC_NUHS), LT(2,KC_4), LT(2,KC_5), LT(2,KC_6), LGUI(KC_MINS),          KC_NO, KC_LPRN, KC_RPRN, KC_NO, KC_NO, KC_NO,
+        KC_LCTL, LT(2,KC_NUBS), LT(2,KC_7), LT(2,KC_8), LT(2,KC_9), LT(2,KC_0),           KC_NO, KC_LBRC, KC_RBRC, KC_COMM, KC_GT, KC_NO,
                                  KC_LSFT, KC_SPC, MO(3), KC_NO, KC_ESC, KC_LALT
         ),
         [3] = LAYOUT_split_3x6_3(
