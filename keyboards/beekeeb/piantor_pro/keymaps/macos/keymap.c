af01b11b57362ec30555dd3370361f1c5f2bbd1d
jocarino <joao.goncalo.veloso@gmail.com>
2025-07-23 11:23:28 +0100

    add cmd+opt on right layer

--- keyboards/beekeeb/piantor_pro/keymaps/macos/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/macos/keymap.c
@@ -137,21 +137,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {          
         [0] = LAYOUT_split_3x6_3(
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,               KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL,
-        KC_LALT, KC_A, KC_S, KC_D, KC_F, KC_G,              KC_H, KC_J, KC_K, KC_L, LT(0,KC_SCLN), LT(0,KC_QUOT),
+        KC_LGUI, KC_A, KC_S, KC_D, KC_F, KC_G,              KC_H, KC_J, KC_K, KC_L, LT(0,KC_SCLN), LT(0,KC_QUOT),
         KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,              KC_N, KC_M, LT(0,KC_COMM), LT(0,KC_DOT), LT(0,KC_SLSH), KC_LGUI,
                     KC_LSFT, HYPR_T(KC_SPC), MO(1), MO(2), LGUI_T(KC_ENT), KC_BSPC
         ),
         [1] = LAYOUT_split_3x6_3(
         KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                    KC_CIRC, KC_AMPR, KC_PAST, LT(1,KC_MINS), LT(1,KC_EQL), LALT(KC_BSPC),
-        KC_LALT, LGUI(KC_A), LGUI(KC_S), KC_TAB, KC_LALT, KC_LSFT,               KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, LT(1,KC_QUOT), LALT(KC_3),
+        KC_LGUI, LGUI(KC_A), LGUI(KC_S), KC_TAB, KC_LALT, KC_LSFT,               KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, LT(1,KC_QUOT), LALT(KC_3),
         KC_LCTL, LGUI(KC_Z), LGUI(KC_X), LGUI(KC_C), LGUI(KC_V), LALT(KC_SPC),     KC_NO, KC_HOME, KC_END, KC_PGUP, KC_PGDN, KC_NO,
                                             KC_LSFT, KC_LGUI, KC_NO, MO(3), KC_ESC, KC_LGUI
         ),
         [2] = LAYOUT_split_3x6_3(
         KC_TAB, KC_GRV, LT(2,KC_1), LT(2,KC_2), LT(2,KC_3), LGUI(KC_PLUS),                 LGUI(LSFT(KC_5)), KC_LCBR, KC_RCBR, KC_NO, KC_NO, KC_NO,
-        KC_LALT, LT(2,KC_NUHS), LT(2,KC_4), LT(2,KC_5), LT(2,KC_6), LGUI(KC_MINS),          LGUI(LSFT(LCTL(KC_4))), KC_LPRN, KC_RPRN, KC_NO, KC_NO, KC_NO,
+        KC_LGUI, LT(2,KC_NUHS), LT(2,KC_4), LT(2,KC_5), LT(2,KC_6), LGUI(KC_MINS),          LGUI(LSFT(LCTL(KC_4))), KC_LPRN, KC_RPRN, KC_NO, KC_NO, KC_NO,
         KC_LCTL, LT(2,KC_BSLS), LT(2,KC_7), LT(2,KC_8), LT(2,KC_9), LT(2,KC_0),           KC_NO, KC_LBRC, KC_RBRC, KC_COMM, KC_GT, KC_NO,
-                                 KC_LSFT, KC_SPC, MO(3), KC_NO, KC_ESC, KC_LALT
+                                 KC_LSFT, LGUI(KC_LALT), MO(3), KC_NO, KC_ESC, KC_LALT
         ),
         [3] = LAYOUT_split_3x6_3(
         QK_BOOT, KC_F1, KC_F2, KC_F3, KC_F4, KC_NO,             KC_WH_U, KC_BRIU,      KC_MNXT, KC_VOLU, KC_PSCR, KC_NO,
